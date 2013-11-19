9743084bbc90e2dd33c9fe1f2ceab4079326d9fb
TheGouger <parth@sentryscientific.com>
2013-11-19 02:09:33 -0500

    Add create endpoint

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+
+app.configure(function() {
+  app.use(express.logger());
+  app.use(express.bodyParser());
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.post('/create', function(request, response) {
+  var name = request.body.name;
+  console.log("Creating app named: %s", name);
 });
 
 var port = process.env.PORT || 5000;
