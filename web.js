5e47b670ecccffcf0d84e7939fec9b13caf7e353
TheGouger <parth@sentryscientific.com>
2013-11-20 12:06:11 -0500

    Port 80

--- web.js
+++ web.js
@@ -163,7 +163,7 @@ var create_contacts = function(request, response, conn) {
 }
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
