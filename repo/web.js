f5b6837dca98c6ac45bca8ec643d4cd2845e3abd
TheGouger <parth@sentryscientific.com>
2013-11-20 03:30:06 -0500

    Web app structure

---
+++ repo/web.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var swig = require("swig");
+var cons = require("consolidate");
+var app = express();
+app.use(express.logger());
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname);
+
+app.get('/', function(request, response) {
+		response.render('index.html', {
+			title: "App"
+		});
+});
+	
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
